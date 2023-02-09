154487928220eef019b938a0bc5a02c8bb84a119
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2023-02-09 18:41:21 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let num = 0,
+      res; 
+  str = str.split('');
+  str.forEach(element => {
+    if(element === "(" || element === "[" || element === "{"){
+      num++;
+    } else if (element === ")" || element === "]" || element === "}"){
+      num --;
+    }
+    if (num < 0){
+      num = false;
+    }
+  });
+  return res =  num === 0 ? true : false
 }
